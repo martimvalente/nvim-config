7b5dbbdb3b2fe2c7a6bc533f9bbdd3350dc32ca7
Martim Valente <martim@probely.com>
2022-05-21 03:03:36 +0100

    inicial file dump

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nvim-config
 my nvim config - which is an andaptation to astrovim
+
+> I will lose of screw my nvim env eventually so this is a safeguard
---
+++ lua/default_theme/plugins/dashboard.lua
@@ -0,0 +1,6 @@
+return {
+  DashboardHeader = { fg = C.red },
+  DashboardShortcut = { fg = C.yellow },
+  DashboardFooter = { fg = C.cyan },
+  DashboardCenter = { fg = C.blue },
+}
